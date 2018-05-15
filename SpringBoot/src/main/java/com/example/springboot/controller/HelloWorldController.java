fe68187b7233d12e3e189a3a36be91d0d1c0a4bb
IS96811 <IS96811@192.168.56.1>
2018-05-15 11:33:35 +0300

    init

---
+++ SpringBoot/src/main/java/com/example/springboot/controller/HelloWorldController.java
@@ -0,0 +1,34 @@
+package com.example.springboot.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String sayHello(){
+		return "Hello Spring Boot!!";
+	}
+	
+	@RequestMapping("/sayMe")
+	public String sayMe(String arg){
+		return "hello " + arg;
+	}
+	
+	@RequestMapping("/add")
+	public String add(Integer number1, Integer number2 ){
+		return "result: " + String.valueOf(number1 + number2);
+	}
+	
+	@RequestMapping(value="/json", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> json(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("result: ", "json value");
+		return map;
+	}
+}
